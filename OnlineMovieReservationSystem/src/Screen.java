b26f2c85bfd307a5284c1d095eeee21874818717
Chanel Rose Baluyos <30874903+chanelbaluyos@users.noreply.github.com>
2020-03-31 23:12:59 +0800

    Update Screen.java
    test

--- OnlineMovieReservationSystem/src/Screen.java
+++ OnlineMovieReservationSystem/src/Screen.java
@@ -1,23 +1,27 @@
 import java.util.*;
-/**
- * @author bianca.gilay
- *
- */
+
 public class Screen {
 
-	public void Screen1A(){
-		Scanner getname = new Scanner(System.in);
-		System.out.print("								ONLINE MOVIE RESERVATION		\n\n"
-				       + "									Welcome!				\n\n\n\n"
-		               + "								Please enter your name: ");
-		String userName = getname.nextLine();
-		getname.close();
-		System.out.println("\n\nPress 1 to Continue");
+	public void Screen1(){
+		Scanner getName = new Scanner(System.in);
+		System.out.print("ONLINE MOVIE RESERVATION\n\n"
+		               + "Please enter your name: ");
+		String userName = getName.nextLine();
+	
+		System.out.print("Welcome, hello " + userName + "!\n\n\n");
+
+		getName.close();
+		System.out.println("What would you like to do?\n\n"
+						+  "Press 1 to View Now Showing! Screen\n"
+						+  "Press 2 to View Reservation\n"
+						+  "Press 3 to Checkout Reservation\n\n");
+		
+		
 	}
 	
 	public static void main(String[] args){
 		Screen s = new Screen();
-		s.Screen1A();
+		s.Screen1();
 	}
 	
 }
