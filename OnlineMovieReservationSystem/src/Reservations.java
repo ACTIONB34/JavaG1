05c9699481986ed96979387364edc15bae20bc9c
candacemadelo <62867453+candacemadelo@users.noreply.github.com>
2020-03-31 16:18:41 +0800

    Added Reservations class

---
+++ OnlineMovieReservationSystem/src/Reservations.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author candace.madelo
+ *
+ */
+public class Reservations {
+	
+	int ticketID;
+	int totalPayment;
+	
+	public Reservations() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
