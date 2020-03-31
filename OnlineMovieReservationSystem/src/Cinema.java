655d08216bfc739dacd13f4104140dba966e773c
Chanel Rose Baluyos <30874903+chanelbaluyos@users.noreply.github.com>
2020-03-31 21:51:06 +0800

    moved docu to screen folder

---
+++ OnlineMovieReservationSystem/src/Cinema.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author chanel.baluyos
+ *
+ */
+public class Cinema {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
