ed3d0d51dcfa77db6805192bb57de78226bccccc
candacemadelo <62867453+candacemadelo@users.noreply.github.com>
2020-03-31 17:10:34 +0800

    Updated Reservation class

--- OnlineMovieReservationSystem/src/Reservations.java
+++ OnlineMovieReservationSystem/src/Reservations.java
@@ -8,11 +8,73 @@
  */
 public class Reservations {
 	
-	int ticketID;
-	int totalPayment;
+	int reservationID;
+	int totalAmount;
+	int seatID;
+	int cinemaID;
+	int customerID;
 	
-	public Reservations() {
-		// TODO Auto-generated constructor stub
+	
+	public Reservations(int reservationID, int totalAmount, int seatID, int cinemaID, int customerID) {
+		this.reservationID = reservationID;
+		this.totalAmount = totalAmount;
+		this.seatID = seatID;
+		this.cinemaID = cinemaID;
+		this.customerID = customerID;
+	}
+
+
+	public int getReservationID() {
+		return reservationID;
+	}
+
+
+	public void setReservationID(int reservationID) {
+		this.reservationID = reservationID;
+	}
+
+
+	public int getTotalAmount() {
+		return totalAmount;
+	}
+
+
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+
+	public int getSeatID() {
+		return seatID;
+	}
+
+
+	public void setSeatID(int seatID) {
+		this.seatID = seatID;
+	}
+
+
+	public int getCinemaID() {
+		return cinemaID;
+	}
+
+	public void setCinemaID(int cinemaID) {
+		this.cinemaID = cinemaID;
+	}
+
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+
+	public void updateReservation() {
+		
 	}
 
 }
