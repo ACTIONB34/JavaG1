3c72072b885a3e1cedf4c499f3686e38437fb192
Adrianne Cris <chanelbaluyos@gmail.com>
2020-04-01 02:09:15 +0800

    Added package, and move displays to Screens.java

--- OnlineMovieReservationSystem/src/Screen.java
+++ OnlineMovieReservationSystem/src/Screen.java
@@ -1,55 +1,34 @@
-import java.util.*;
+import com.aws.JavaG1.Screens;
 
 public class Screen {
 
-	public String Screen1A(){
-		Scanner obj = new Scanner(System.in);
-		System.out.print("ONLINE MOVIE RESERVATION\n\n"
-		               + "Please enter your name: ");
-		return obj.nextLine();
-	}
-	
-	public int Screen1B(String name) {
-		Scanner obj = new Scanner(System.in);
-		System.out.print("Welcome, hello " + name + "!\n\n\n");
 
-		
-		System.out.println("What would you like to do?\n\n"
-						+  "Press 1 to View Now Showing! Screen\n"
-						+  "Press 2 to View Reservation\n"
-						+  "Press 3 to Checkout Reservation\n\n");
-		int choice = obj.nextInt();
-		
-		return choice;
-	
-	}
-	
-	public static void main(String[] args){
-		String username;
-		int choice;
-		
-		Screen s = new Screen();
-		
-		username = s.Screen1A();
-		choice = s.Screen1B(username);
-		
-		switch (choice){
-		case 1:
-			System.out.println("1");
-			break;
-		case 2:
-			System.out.println("2");
-			break;
-		case 3:
-			System.out.println("3");
-			break;
-		case 0:
-			System.out.println("Bye!");
-		}
-		
-		
-		/*for checking*/
-		System.out.println(username + " " + choice);
-	}
-	
+    public static void main(String[] args) {
+        String Name = "";
+        int choice = -1;
+        Name = Screens.Screen1A();
+
+        while (choice != 0) {
+            choice = Screens.Screen1B(Name);
+            switch (choice) {
+                case 1:
+					System.out.println("1");
+                    break;
+                case 2:
+					System.out.println("2");
+					break;
+                case 3:
+					System.out.println("3");
+					break;
+                case 0:
+                    break;
+                default:
+					System.out.println("INVALID CHOICE!\n");
+                    break;
+            }
+        }
+
+
+    }
+
 }
