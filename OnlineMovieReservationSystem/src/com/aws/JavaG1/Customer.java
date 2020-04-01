ce0db1bb8f371d520041f5f055681422d19ad514
Andre-ops <andre.maraton00@gmail.com>
2020-04-01 20:05:45 +0800

    Update Customer.java

--- OnlineMovieReservationSystem/src/com/aws/JavaG1/Customer.java
+++ OnlineMovieReservationSystem/src/com/aws/JavaG1/Customer.java
@@ -80,19 +80,23 @@ public class Customer {
 		}
 	}
 	
-	public void checkOut(String customer){
+	public void reserveTicket(int cinema_id, String time, int customer_id, double total_payment,int noOfKids, int noOfAdults, int noOfSeniors){
+		//create database connection
 		Connection conn = null;
 		Statement stmt = null;
-		ResultSet rs = null;
+		
 		try {
-		    conn =
-		       DriverManager.getConnection("jdbc:mysql://localhost/movie?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+			//open database 
+		    conn = DriverManager.getConnection("jdbc:mysql://localhost/movie?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
 		    		   "root", "awsys+123");
-
+		    
+		    //Saving data into the database
 		    stmt = conn.createStatement();
-		    stmt.executeUpdate("INSERT INTO reservation ('')"
-		            +"VALUES ()");
+		    stmt.executeUpdate("INSERT INTO reservation (cinema_id,time,customer_id,total_payment, no_of_kid, no_of_adult, no_of_senior)"
+		            +"VALUES (cinema_id, time, customer_id,total_payment, noOfKids, noOfAdults, noOfSeniors)");
 
+		    //close database
+		    conn.close();
 		} catch (SQLException ex) {
 		    // handle any errors
 		    System.out.println("SQLException: " + ex.getMessage());
