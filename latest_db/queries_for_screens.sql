62cdea65f876d3a850401dbb5c1b1ed6e72d7a5c
Chanチャン <33647503+kryschanhi@users.noreply.github.com>
2020-04-01 16:30:39 +0800

    Update queries_for_screens.sql

--- latest_db/queries_for_screens.sql
+++ latest_db/queries_for_screens.sql
@@ -1,6 +1,9 @@
+/*don't execute as a whole, execute per query (please look at the comments).*/
+
+/*cinemas and movies*/
 USE moviereservation;
 
-SELECT	
+SELECT  
     cinema_id,
     movie_name,
     movie_director,
@@ -12,8 +15,8 @@ SELECT
 /*timeslots*/
 USE moviereservation;
 
-SELECT	
-	movies.movie_id,
+SELECT  
+    movies.movie_id,
     time_start
     FROM movies, timeslots
     where movies.movie_id = timeslots.movie_id;
@@ -21,10 +24,30 @@ SELECT
 /*unreserved seats*/
 USE moviereservation;
 
-SELECT	
-	seat_number
+SELECT  
+    seat_number
     FROM seats
     WHERE timeslot_id = 1 AND cinema_id = 1 
     AND reservation_id IS NULL;
 
-/*timeslot_id is changeable and cinema_id is changeable*/
+/*timeslot_id is changeable and cinema_id is changeable
+ids need to be pulled from db based on the user's choices
+*/
+
+/*insert reservation*/
+USE moviereservation;
+
+INSERT INTO reservations 
+    (cinema_id, 
+    time, 
+    customer_name, 
+    total_payment, 
+    no_of_kid,
+    no_of_adult,
+    no_of_senior,
+    timeslot_id)
+VALUES
+    (1,'12:00PM','Johnny Test',150,0,1,0,1);
+    
+/*everything is changable, ids need to be pulled from db based on the user's choices*/
+/*date is TIMESTAMP, ID is auto increment*/
