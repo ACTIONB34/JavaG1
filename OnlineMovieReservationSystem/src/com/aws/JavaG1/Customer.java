bc4fd1a5fd546716357d13e00f7a0d0fd4b38e88
Andre-ops <andre.maraton00@gmail.com>
2020-04-04 13:55:06 +0800

    Update Customer.java

--- OnlineMovieReservationSystem/src/com/aws/JavaG1/Customer.java
+++ OnlineMovieReservationSystem/src/com/aws/JavaG1/Customer.java
@@ -56,58 +56,6 @@ public class Customer {
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
-	
-	public void viewMovies(){
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
-		    conn =
-		       DriverManager.getConnection("jdbc:mysql://localhost/movie?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-		    		   "root", "awsys+123");
-
-		    stmt = conn.createStatement();
-		    rs = stmt.executeQuery("select * from movie;");
-		    
-		    while(rs.next()) {
-		    	System.out.println(rs.getString(2));
-		    }
-		    
-		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-	}
-	
-	public void reserveTicket(){
-		int cinemaID = 1;
-		String time = "10:00 AM - 12:00 PM, Nov-20-2020";
-		int customerID = 1;
-		double totalPayment = 1150.00;
-		int noOfKids = 5;
-		int noOfAdults = 5;
-		int noOfSeniors = 5;
-		
-		try {
-			Connection conn = null;
-			Statement stmt = null;
-			
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/pet_clinic?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-		    		   "root", "awsys+123");
-			
-			stmt = conn.createStatement();
-			stmt.executeUpdate("INSERT INTO `reservation`(cinemaID,time,customerID,totalPayment,noOfKids,noOfAdults,noOfSeniors) "
-		    		+ "VALUE ('"+cinemaID+"','"+time+"','"+customerID+"',"+totalPayment+",'"+noOfKids+"','"+noOfAdults+"','"+noOfSeniors+"')");
-
-		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-	}
 }
 
 
